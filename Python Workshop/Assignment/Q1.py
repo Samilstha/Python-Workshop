7ecee9f220933fcf58736bad9181e0a164c93e38
Nishesh Thakuri <46592704+nthakuri56@users.noreply.github.com>
2019-01-18 21:17:38 +0545

    Delete Q1.py

--- Python Workshop/Assignment/Q1.py
+++
@@ -1,8 +0,0 @@
-Q1. The volume of a sphere with radius r is (4/3)*π*r^3. What is the volume of a sphere with radius 5?
-
-Solution:
-    
-π = 3.14
-r= 5
-V= (4/3)*π* r**3
-print('The volume of the sphere with radius 5 is: ',V)
