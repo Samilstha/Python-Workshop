69a10dcf32ba4bcb20a4d4e9a0fe590b0c20c4c8
Nishesh Thakuri <46592704+nthakuri56@users.noreply.github.com>
2019-01-21 20:13:14 +0545

    Update Q7.py

--- JAN 12/Assignment/Q7.py
+++ JAN 12/Assignment/Q7.py
@@ -1,3 +1,13 @@
+Write a function called disect that takes a list, modifies it by removing the first and
+last elements
+For example:
+>>> l = [1, 2, 3, 4]
+>>> disect(t)
+>>> t
+[2, 3]
+
+Solution:
+
 def disect(t):
     del t[0]
     if(len(t)-1>0):
