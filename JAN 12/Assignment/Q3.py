c0e91c44c338c82acceb00abf21d80a87787df73
Nishesh Thakuri <46592704+nthakuri56@users.noreply.github.com>
2019-01-21 20:08:41 +0545

    Update Q3.py

--- JAN 12/Assignment/Q3.py
+++ JAN 12/Assignment/Q3.py
@@ -1,3 +1,8 @@
+We have v=u+at where v is velocity, u is initial velocity, and t is the time. And value for each is as
+follows: v=25m/s. u=0m/s, t=10 seconds. Find the acceleration a.
+
+Solution:
+  
 v = 25 
 u = 0 
 t = 10
