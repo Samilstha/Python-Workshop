096168110a3a7f59cafecdb2289e1bca72949eb7
Nishesh Thakuri <46592704+nishesh99@users.noreply.github.com>
2019-02-02 21:50:28 +0545

    Update QXI.py

--- JAN 26/Assignment/QXI.py
+++ JAN 26/Assignment/QXI.py
@@ -1,9 +1,19 @@
+Q. Write a function that reads a text file words.txt and generate a dictionary that stores each
+word as a key and number of letters in that word as a value. For example:
+your words.txt file contains words like: Hello Python.
+The output of your program should be dictionary like this: {“Hello”: 5, “Python”: 6}
+Solution:
 
-def histogram(s):
-    d = dict()
-    for c in s:
-        if c not in d:
-            d[c] = 1
-        else:
-            d[c] = d[c] + 1
-    return d
+def gen_dic(name):
+    dict_1 = {}
+    f = open(name, 'r')
+    for x in f:
+        list_1 = x.split()
+        for each in list_1:
+            dict_1[each] = len(each)
+        list_1 = []
+    return dict_1
+
+
+fname = input("Enter file name: ")
+print(f"{gen_dic(fname)}")
